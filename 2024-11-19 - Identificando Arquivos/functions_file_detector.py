2a90d0a1b3421d55a912fcbb5e89e29093e57583
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2024-11-19 16:45:46 -0300

    Detector de Tipo de Arquivo

--- 2024-11-19 - Identificando Arquivos/functions_file_detector.py
+++ 2024-11-19 - Identificando Arquivos/functions_file_detector.py
@@ -1,18 +1,25 @@
+import sys 
+
 from file_type_signatures import *
 
 # ----------------------------------------------------------------------
 def getTipoArquivo(nomeArquivo: str):
    intMaxTamanho = max(len(strAssinatura) for strAssinatura in ASSINATURAS_ARQUIVOS.keys())
 
-   with open(nomeArquivo, 'rb') as file:
-      headerArquivo = file.read(intMaxTamanho)
+   try:
+      with open(nomeArquivo, 'rb') as file:
+         headerArquivo = file.read(intMaxTamanho)
+   except FileNotFoundError:
+      raise FileNotFoundError(f'Arquivo {nomeArquivo.upper()} não encontrado...')
+   except:
+      raise Exception(f'ERRO: {sys.exc_info()[0]}')
+   else:
+      for strAssinatura, strTipoArquivo in ASSINATURAS_ARQUIVOS.items():
+         if headerArquivo.startswith(strAssinatura):
+            return strTipoArquivo
 
-   for strAssinatura, strTipoArquivo in ASSINATURAS_ARQUIVOS.items():
-      if headerArquivo.startswith(strAssinatura):
-         return strTipoArquivo
+      strExtensaoArquivo = '.' + nomeArquivo.split('.')[-1]
+      if strExtensaoArquivo in EXTENSOES_LINGUAGENS.keys():
+         return EXTENSOES_LINGUAGENS[strExtensaoArquivo]
 
-   strExtensaoArquivo = '.' + nomeArquivo.split('.')[-1]
-   if strExtensaoArquivo in EXTENSOES_LINGUAGENS.keys():
-      return EXTENSOES_LINGUAGENS[strExtensaoArquivo]
-
-   return 'Tipo de Arquivo Desconhecido'
+      return 'Tipo de Arquivo Desconhecido'
