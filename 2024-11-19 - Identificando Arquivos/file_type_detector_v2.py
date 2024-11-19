707af5651f5d0f1633bf6e55745f5e19291c6df5
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2024-11-19 17:18:21 -0300

    Detector de Tipo de Arquivo

---
+++ 2024-11-19 - Identificando Arquivos/file_type_detector_v2.py
@@ -0,0 +1,18 @@
+import os, sys
+
+from functions_file_detector import *
+
+strDirArquivo  = f'{os.path.dirname(os.path.realpath(__file__))}\\arquivos'
+
+lstArquivos = [f for f in os.listdir(strDirArquivo) if os.path.isfile(os.path.join(strDirArquivo, f))]
+
+for strArquivo in lstArquivos:
+   try:
+      strTipoArquivo = getTipoArquivo(f'{strDirArquivo}\\{strArquivo}')
+   except FileNotFoundError:
+      print(f'\nArquivo {strArquivo.upper()} não encontrado...\n')
+   except:
+      print(f'\nERRO: {sys.exc_info()[0]}\n')
+   else:
+      print(f'\nArquivo: {strArquivo}')
+      print(f'Tipo...: {strTipoArquivo}\n')
