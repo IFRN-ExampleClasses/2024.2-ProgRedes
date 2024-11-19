e412a2715394c6f0a7f58173e5c5a72831520ea6
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2024-11-19 17:41:56 -0300

    Detector de Tipo de Arquivo

--- 2024-11-19 - Identificando Arquivos/functions_file_detector.py
+++ 2024-11-19 - Identificando Arquivos/functions_file_detector.py
@@ -4,15 +4,25 @@ from file_type_signatures import *
 
 # ----------------------------------------------------------------------
 def getTipoArquivoOffice365(nomeArquivo: str):
-    with zipfile.ZipFile(nomeArquivo, 'r') as zip:
-        if 'word/document.xml' in zip.namelist():
-            return "Documento Word (.docx)"
-        elif 'xl/workbook.xml' in zip.namelist():
-            return "Planilha Excel (.xlsx)"
-        elif 'ppt/presentation.xml' in zip.namelist():
-            return "Apresentação PowerPoint (.pptx)"
-        else:
-            return "Tipo desconhecido"
+   with zipfile.ZipFile(nomeArquivo, 'r') as zip:
+      if 'word/document.xml' in zip.namelist():
+         return 'Documento Word (.docx)'
+      elif 'xl/workbook.xml' in zip.namelist():
+         return 'Planilha Excel (.xlsx)'
+      elif 'ppt/presentation.xml' in zip.namelist():
+         return 'Apresentação PowerPoint (.pptx)'
+      elif 'visio/document.xml' in zip.namelist():
+         return 'Desenho Visio (.vsdx)'
+      elif 'project/project.xml' in zip.namelist():
+         return 'Projeto Project (.mpp)'
+      elif 'onenote/document.xml' in zip.namelist():
+         return 'Bloco de Notas OneNote (.one)'
+      elif 'outlook/item1.xml' in zip.namelist():
+         return 'Email Outlook (.msg)'
+      elif 'access/document.xml' in zip.namelist():
+         return 'Banco de Dados Access (.accdb)'
+      else:
+         return "Tipo desconhecido"
         
 # ----------------------------------------------------------------------
 def getTipoArquivo(nomeArquivo: str):
